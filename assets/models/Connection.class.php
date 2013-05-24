264ecac9c25d81b8eea135f087e950f3c50ccd00
Gabriel Santerre <gabrielsanterre@gmail.com>
2013-05-24 00:45:31 -0400

    ajout de constante de connection

--- assets/models/Connection.class.php
+++ assets/models/Connection.class.php
@@ -4,8 +4,13 @@
 
 		public static $db;
 
-		public function __construct(){
-			Connection::$db = new MysqliDb('localhost', 'root', 'admin', 'tpweb');
+		const SERVER = "localhost";
+		const USER = "root";
+		const PW = "admin";
+		const DB = "tpweb";
+
+		public function __construct($pServer=self::SERVER, $pUser=self::USER, $pPassword=self::PW, $pDB=self::DB){
+			Connection::$db = new MysqliDb($pServer, $pUser, $pPassword, $pDB);
 		}
 
 	}
